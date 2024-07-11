2c8ae4f0f35b7187092f96a3b9fc6bf05a66f0e4
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-07-11 12:48:00 +0300

    update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,35 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
-const app = express();
-
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = Number(process.env.PORT);
-app.listen();
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
