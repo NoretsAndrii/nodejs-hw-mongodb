d16c843f821d08d29c74cdcdfe95c54de518a166
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-07-28 10:25:53 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -12,7 +12,7 @@ export const getAllContacts = async ({
   const skip = (page - 1) * perPage;
 
   const contactsQuery = ContactsCollection.find();
-  console.log(filter);
+
   if (filter.contactType) {
     contactsQuery.where('contactType').equals(filter.contactType);
   }
